9eff576edab856e15a7fb272f31a6e22ea7b7add
junwoochoi <cjw4690@gmail.com>
2019-12-26 17:36:51 +0900

    feat : 크롤 로깅

--- src/main/kotlin/com/example/crawler/crawler/WebCrawler.kt
+++ src/main/kotlin/com/example/crawler/crawler/WebCrawler.kt
@@ -2,14 +2,19 @@ package com.example.crawler.crawler
 
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class WebCrawler {
 
+    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     @Scheduled(initialDelay = 0, fixedDelay = 12000)
     fun crawl() {
+        logger.info("크롤 시작!")
         val url = "https://eomisae.co.kr/os"
         val doc = Jsoup.connect(url).get()
         val selector = "#L_ > div._bd.cf.clear > div.card_wrap > div > * > div > div.card_content > h3 > a"
